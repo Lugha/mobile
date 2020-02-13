990625079873762bd722c18f793177f221c73eed
Jalil Taibi <jaliltaibi@MacBook-Pro-de-Manel.local>
2020-02-13 13:15:34 +0100

    socket

--- App.js
+++ App.js
@@ -1,25 +1,45 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Alert } from 'react-native';
+import { StyleSheet, Text, View, TextInput} from 'react-native';
 import { Button } from 'native-base';
 import io from 'socket.io-client';
 
 
 export default class App extends Component {
-  componentDidMount() {
-    const socket = io("http://192.168.0.30:5001/");
+  state = {
+    input: null,
+    sentence: null
   }
-  
+
+  componentDidMount() {
+    this.socket = io("http://192.168.0.30:5001/", { "forceBase64" : 1 });
+
+    this.socket.on("receiveSentence", sentence => { this.setState({ sentence }); console.log("sentence: ", this.state.sentence) } );
+  }
+
+  sendMessage = () => { console.log("in sendMessage"); this.socket.emit("sendSentence", this.state.input)}
+  handleChange = (input) => { this.setState({ input })}
+
+
   render () {
     return (
     <View style={styles.container}>
+      <Text>
+        { this.state.sentence }
+      </Text>
+      <TextInput
+        style={styles.input}
+        value={ this.state.input }
+        onChangeText={(value) => this.handleChange(value)}
+      />
       <Button
-        light
+        success
+        onPress={this.sendMessage}
       >
-        <Text> Connect </Text>
+        <Text> Envoyer </Text>
       </Button>
     </View>
     )
-  };
+  }; 
 }
 
 const styles = StyleSheet.create({
@@ -29,4 +49,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    height: 30,
+    width: "80%",
+    borderColor: 'gray',
+    borderWidth: 1
+  }
 });
